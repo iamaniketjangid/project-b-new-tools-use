384cbb33f21045bd537e6fc2e1a12f1bab44b18b
Aniket Jangid <anikettech1460@gmail.com>
2026-01-08 18:51:45 +0530

    Second trigger: API text change

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (_req, res) => {
-  res.send('Project B – ci trigerffff test');
+  res.send('Project B – ci new1 commit test');
 });
 
 app.listen(3000, () => {
