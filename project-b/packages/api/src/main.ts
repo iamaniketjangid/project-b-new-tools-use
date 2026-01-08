aca9985e0bef8c116f8c6e4c08c8bd24e1475c7c
Aniket Jangid <anikettech1460@gmail.com>
2026-01-08 18:14:29 +0530

    Project B: Nx + Buildpacks + fast CI pipeline

---
+++ project-b/packages/api/src/main.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (_req, res) => {
+  res.send('Project B – Modern DevOps');
+});
+
+app.listen(3000, () => {
+  console.log('Project B API running on port 3000');
+});
+
